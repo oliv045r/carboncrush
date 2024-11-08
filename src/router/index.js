2ee7bfeba18dd4921adfdccc745bf7f6b485dee2
Oliver Hansen <oliv045r@edu.zealand.dk>
2024-11-08 09:06:45 +0100

    Slettet ubrugte sider

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,14 @@ import StartScreen from '@/views/StartScreen.vue';
 import BackgroundSelect from '@/views/BackgroundSelect.vue';
 import ImageFormat from '@/views/ImageFormat.vue';
 import IconFormat from '@/views/IconFormat.vue';
-import FeedbackIcon from '@/components/feedback/FeedbackIcon.vue';
-import FeedbackTemplate from '@/components/feedback/FeedbackTemplate.vue';
-import FeedbackIconBad from '@/components/feedback/FeedbackIconBad.vue';
 import AppHeader from '@/components/navigation/TopNav.vue';
 import GameInformation from '@/views/GameInformation.vue';
-import FeedbackImage from '@/components/feedback/FeedbackImage.vue';
 import ScoreDisplay from '@/components/ScoreDisplay.vue'; // Ensure this component exists
 import EndGame from '@/views/EndGame.vue';
 import AnimationSelect from '@/views/AnimationSelect.vue'; // Ny import til Animation
 import ImageQuality from '../views/ImageQuality.vue'; // Ny import til ImageQuality
 import FontColorSelect from '../views/FontColorSelect.vue'; // Korriger sti til FontColorSelect
 import FontSelect from '../views/FontSelect.vue'; // Korriger sti til FontdSelect
-import FeedbackBackground from '../components/feedback/FeedbackBackground.vue'; // Korriger sti til FeedbackBackground
-import FeedbackFont from '@/components/feedback/FeedbackFont.vue';
 import FeedbackPop from '@/components/feedback/FeedbackPop.vue';
 import FeedbackPopIcon from '@/components/feedback/FeedbackPopIcon.vue';
 import FeedbackPopIconBad from '@/components/feedback/FeedbackPopIconBad.vue';
@@ -57,11 +51,7 @@ const routes = [
     name: 'IconFormat',
     component: IconFormat,
   },
-  {
-    path: '/feedback-icon',
-    name: 'FeedbackIcon',
-    component: FeedbackIcon,
-  },  
+
   {
     path: '/feedback-pop',
     name: 'FeedbackPop',
@@ -77,27 +67,6 @@ const routes = [
     name: 'FeedbackPopIconBad',
     component: FeedbackPopIconBad,
   }, 
-  {
-    path: '/feedback-Background',
-    name: 'FeedbackBackground',
-    component: FeedbackBackground,
-  },
-  {
-    path: '/feedback-font',
-    name: 'FeedbackFont',
-    component: FeedbackFont,
-  },
-  
-  {
-    path: '/feedback-icon-bad',
-    name: 'FeedbackIconBad',
-    component: FeedbackIconBad,
-  },
-  {
-    path: '/feedback-template',
-    name: 'FeedbackTemplate',
-    component: FeedbackTemplate,
-  },
   {
     path: '/header-component',
     name: 'AppHeader',
@@ -117,11 +86,6 @@ const routes = [
     path: '/article-single',
     name: 'ArticleSingle',
     component: ArticleSingle,
-  },
-  {
-    path: '/feedback-image',
-    name: 'FeedbackImage',
-    component: FeedbackImage,
   },
   {
     path: '/end-game',
